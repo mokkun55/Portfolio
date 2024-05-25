6a30907923d1eca1d97d271c40cf83841e71955e
mokkun55 <mokkunpc@gmail.com>
2024-05-25 19:26:57 +0900

    anime

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,20 @@
+import { motion } from "framer-motion";
 import React from "react";
 
 function Hero() {
   return (
     <div className="hero">
-      <div className="profile">
+      <motion.div
+        className="profile"
+        initial={{ y: "100vh" }}
+        animate={{ y: 0 }}
+      >
         <img src="/imgs/about/profIcon.jpg" alt="icon" className="MyIcon" />
         <div className="description">
           <h1>mokkun</h1>
           <p>Hello, World!</p>
         </div>
-      </div>
+      </motion.div>
       <div class="area">
         <ul class="circles">
           <li></li>
